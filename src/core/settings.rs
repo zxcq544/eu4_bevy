c96f1eeff199feafa3dadce820f56a835b1e3d87
Sergey Anisimov <zxcq544@yandex.ru>
2026-07-17 04:38:42 +0300

    use default steam dir

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -6,27 +6,25 @@ pub struct Settings {
 }
 
 pub fn get_eu4_settings() -> Settings {
-    let settings_file = "./settings.json";
-    let settings: Settings = if std::path::Path::new(settings_file).exists() {
+    let settings_filename = "./settings.json";
+    let steam_default_eu4_folder: &str =
+        "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Europa Universalis IV";
+    let settings: Settings = if std::path::Path::new(settings_filename).exists() {
         // Load settings from file
-        let settings_file = std::fs::File::open(settings_file).unwrap();
+        let settings_file = std::fs::File::open(settings_filename).unwrap();
         serde_json::from_reader(settings_file).unwrap()
     } else {
-        // Create settings file
-        let settings_file = std::fs::File::create(settings_file).unwrap();
-        serde_json::to_writer_pretty(
-            settings_file,
-            &Settings {
-                eu4_folder:
-                    "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Europa Universalis IV"
-                        .to_string(),
-            },
-        )
-        .unwrap();
-        Settings {
-            eu4_folder: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Europa Universalis IV"
-                .to_string(),
-        }
+        // Create settings file with default values
+        println!(
+            "ℹ️ Settings file \'{0}\' doesn't exist, creating default settings",
+            settings_filename
+        );
+        let settings_file = std::fs::File::create(settings_filename).unwrap();
+        let default_settings = Settings {
+            eu4_folder: steam_default_eu4_folder.to_string(),
+        };
+        serde_json::to_writer_pretty(settings_file, &default_settings).unwrap();
+        return default_settings;
     };
     // if eu4 folder from settings.json doesn't exist on disk - panic
     if !std::path::Path::new(&settings.eu4_folder).exists() {
