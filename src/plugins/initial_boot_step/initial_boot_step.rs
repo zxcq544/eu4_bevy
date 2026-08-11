a61aef0f81ca806b81bc8aed5204234070f0e933
Sergey Anisimov <zxcq544@yandex.ru>
2026-08-11 08:11:47 +0300

    set default cursor

--- src/plugins/initial_boot_step/initial_boot_step.rs
+++ src/plugins/initial_boot_step/initial_boot_step.rs
@@ -1,10 +1,12 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{CursorIcon, CustomCursor, CustomCursorImage},
+};
 
 use crate::states::GameState;
 pub struct InitialBootStepPlugin;
 /// This plugin is responsible for setting up the initial boot step
-/// Usually this could be used to calculate checksums and so on.
-/// Currently just empty.
+/// This one sets default cursor and initial background and camera
 impl Plugin for InitialBootStepPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, initial_boot_step);
@@ -13,11 +15,26 @@ impl Plugin for InitialBootStepPlugin {
 
 pub fn initial_boot_step(
     mut commands: Commands,
+    window: Single<Entity, With<Window>>,
     current_state: Res<State<GameState>>,
     mut next_state: ResMut<NextState<GameState>>,
+    asset_server: Res<AssetServer>,
 ) {
     info!("current state is {:?}", current_state.get());
     info!("initial boot step");
+    let cursor_handle = asset_server.load("gfx/cursors/normal.png");
+
+    commands
+        .entity(*window)
+        .insert(CursorIcon::Custom(CustomCursor::Image(CustomCursorImage {
+            handle: cursor_handle,
+            hotspot: (0, 0),
+            texture_atlas: None,
+            flip_x: false,
+            flip_y: false,
+            rect: None,
+            ..default()
+        })));
     next_state.set(GameState::LoadingAssets);
     // commands.spawn(Camera3dBundle {
     //     transform: Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
