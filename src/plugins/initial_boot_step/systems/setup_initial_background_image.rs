d4cd01a3cbecafd36bf9a6e7d22e1c09ac220d2a
Sergey Anisimov <zxcq544@yandex.ru>
2026-08-17 11:30:22 +0300

    refactor remove unused code

--- src/plugins/initial_boot_step/systems/setup_initial_background_image.rs
+++ src/plugins/initial_boot_step/systems/setup_initial_background_image.rs
@@ -6,58 +6,35 @@ use bevy::prelude::*;
 
 pub fn setup_initial_background_image(
     mut commands: Commands,
-    background_image: Res<InitialBootingBackgroundScreen>,
+    background_image_res: Res<InitialBootingBackgroundScreen>,
 ) {
     info!("Setting initial background image");
     commands.spawn((Camera2d::default(), MainMenuBackground));
-    // TODO: try to convert to bsn! and check how to free resources and components and so on
-    commands
-        .spawn((
-            MainMenuBackground,
-            Node {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                position_type: PositionType::Absolute,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                overflow: Overflow::hidden(),
-                ..default()
-            },
-        ))
-        .with_children(|parent| {
-            parent.spawn((
-                Node {
-                    width: Val::Percent(100.0),
-                    height: Val::Auto,
-                    ..default()
-                },
-                ImageNode {
-                    image: background_image.image.clone(),
-                    ..default()
-                },
-            ));
-        });
-    // commands.spawn_scene(ui());
+    // TODO: check how to free resources and components and so on
+    commands.spawn_scene(ui(background_image_res));
 }
 
-// fn ui() -> impl Scene {
-//     bsn! {
-//         MainMenuBackground
-//         Node{
-//             width: Val::Percent(100.0),
-//             height: Val::Percent(100.0),
-//             position_type: PositionType::Absolute,
-//             justify_content: JustifyContent::Center,
-//             align_items: AlignItems::Center,
-//             overflow: Overflow::hidden()
-//         }
-//         Children[
-//             Node{
-//                 width: Val::Percent(100.0),
-//             }
-//             ImageNode{
-//                 image: "gfx/loadingscreens/load_0.dds"
-//             }
-//         ]
-//     }
-// }
+fn ui(background_image_res: Res<InitialBootingBackgroundScreen>) -> impl Scene {
+    let background_image = background_image_res.image.clone();
+    bsn! {
+        MainMenuBackground
+        Node{
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            position_type: PositionType::Absolute,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            overflow: Overflow::hidden()
+        }
+        Children[
+            Node{
+                width: Val::Percent(100.0),
+                height: Val::Auto,
+            }
+            ImageNode{
+                image: background_image,
+                image_mode: NodeImageMode::Auto,
+            }
+        ]
+    }
+}
