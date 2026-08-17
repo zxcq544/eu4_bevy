2c36bf291095f205bdebac8aa5c34ef4861464f2
Sergey Anisimov <zxcq544@yandex.ru>
2026-08-17 14:12:04 +0300

    move scene craetion near entity

--- src/plugins/initial_boot_step/components/initial_boot_entity.rs
+++ src/plugins/initial_boot_step/components/initial_boot_entity.rs
@@ -2,3 +2,29 @@ use bevy::prelude::*;
 
 #[derive(Component, Default, Clone)]
 pub struct InitialBootEntity;
+
+impl InitialBootEntity {
+    pub fn as_scene(background_image: Handle<Image>) -> impl Scene {
+        bsn! {
+            InitialBootEntity
+            Node{
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                overflow: Overflow::hidden()
+            }
+            Children[
+                Node{
+                    width: Val::Percent(100.0),
+                    height: Val::Auto,
+                }
+                ImageNode{
+                    image: background_image,
+                    image_mode: NodeImageMode::Auto,
+                }
+            ]
+        }
+    }
+}
--- src/plugins/initial_boot_step/systems/setup_initial_background_image.rs
+++ src/plugins/initial_boot_step/systems/setup_initial_background_image.rs
@@ -11,30 +11,7 @@ pub fn setup_initial_background_image(
     info!("Setting initial background image");
     commands.spawn((Camera2d::default(), InitialBootEntity));
     // TODO: check how to free resources and components and so on
-    commands.spawn_scene(ui(background_image_res));
-}
-
-fn ui(background_image_res: Res<InitialBootingBackgroundScreen>) -> impl Scene {
-    let background_image = background_image_res.image.clone();
-    bsn! {
-        InitialBootEntity
-        Node{
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            position_type: PositionType::Absolute,
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            overflow: Overflow::hidden()
-        }
-        Children[
-            Node{
-                width: Val::Percent(100.0),
-                height: Val::Auto,
-            }
-            ImageNode{
-                image: background_image,
-                image_mode: NodeImageMode::Auto,
-            }
-        ]
-    }
+    commands.spawn_scene(InitialBootEntity::as_scene(
+        background_image_res.image.clone(),
+    ));
 }
