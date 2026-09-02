5e7d279a103010c7c07b4e5aedc1b07c0ba40f06
Sergey Anisimov <zxcq544@yandex.ru>
2026-09-02 19:11:01 +0300

    hide menu

--- src/plugins/main_menu/systems/spawn_options_block.rs
+++ src/plugins/main_menu/systems/spawn_options_block.rs
@@ -1,5 +1,9 @@
 use crate::plugins::main_menu::{
-    components::options_entity::OptionsEntity, resources::options_images::OptionsImages,
+    components::{
+        continue_game_entity::ContinueGameEntity, main_menu_entity::MainMenuEntity,
+        options_entity::OptionsEntity,
+    },
+    resources::options_images::OptionsImages,
 };
 use bevy::prelude::*;
 use bevy_fluent::Localization;
@@ -10,6 +14,7 @@ pub fn spawn_options_block(
     fonts: Res<FontHandles>,
     localization_res: Res<Localization>,
     options_images: Res<OptionsImages>,
+    mut query: Query<&mut Visibility, Or<(With<MainMenuEntity>, With<ContinueGameEntity>)>>,
 ) {
     info!("Spawning options block");
     commands.spawn_scene_list(OptionsEntity::as_scene_list(
@@ -17,4 +22,17 @@ pub fn spawn_options_block(
         &localization_res,
         &fonts,
     ));
+    // hide main menu block and continue game block
+    for mut visibility in &mut query {
+        *visibility = Visibility::Hidden;
+    }
 }
+
+// fn hide_main_menu_block(
+//     commands: &mut Commands,
+//     mut query: Query<&mut Visibility, With<MainMenuEntity>>,
+// ) {
+//     for mut visibility in &mut query {
+//         *visibility = Visibility::Hidden;
+//     }
+// }
