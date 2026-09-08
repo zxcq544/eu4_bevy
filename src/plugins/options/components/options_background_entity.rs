034acead16ef6bdb1c0b88bae76be01ca3c4db6d
Sergey Anisimov <zxcq544@yandex.ru>
2026-09-08 05:23:22 +0300

    try as commands because scenes don't have if else yet

--- src/plugins/options/components/options_background_entity.rs
+++ src/plugins/options/components/options_background_entity.rs
@@ -44,4 +44,47 @@ impl OptionsBackgroundEntity {
             ]
         }
     }
+    pub fn spawn_using_commands(mut commands: Commands, options_images: Res<OptionsImages>) {
+        commands
+            .spawn((
+                OptionsBackgroundEntity,
+                Node {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    position_type: PositionType::Absolute,
+                    justify_content: JustifyContent::Center,
+                    // background_color: Color::TRANSPARENT,
+                    // overflow: Overflow::hidden(),
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                Visibility::Hidden, // Spawn as hidden because we don't despawn ingame ui elements
+                ZIndex(1),
+                BackgroundColor(Color::NONE),
+            ))
+            .with_children(|parent| {
+                parent.spawn((
+                    Node {
+                        display: Display::Flex,
+                        flex_direction: FlexDirection::Column,
+                        width: Val::Percent(70.0),
+                        height: Val::Vh(90.0),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        // bottom: Val::Px(3.0),
+                        ..default()
+                    },
+                    Outline {
+                        color: Color::srgb_from_array([0.4, 0.7, 0.5]),
+                        width: Val::Px(2.0),
+                        ..default()
+                    },
+                    ImageNode {
+                        image: options_images.settings_bg_image.clone(),
+                        image_mode: NodeImageMode::Stretch,
+                        ..default()
+                    },
+                ));
+            });
+    }
 }
