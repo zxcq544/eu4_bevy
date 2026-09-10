753066917d174082e305e41187e65840204fa93a
Sergey Anisimov <zxcq544@yandex.ru>
2026-09-10 10:23:55 +0300

    refactor format

--- src/plugins/options/systems/options_middle_block_systems/mod.rs
+++ src/plugins/options/systems/options_middle_block_systems/mod.rs
@@ -1,5 +1,5 @@
 pub mod audio_volume_update_system;
 pub mod hide_options_audio_tab;
 pub mod hide_options_video_tab;
-pub mod set_display_grid_options_audio_tab;
 pub mod set_display_flex_options_video_tab;
+pub mod set_display_grid_options_audio_tab;
