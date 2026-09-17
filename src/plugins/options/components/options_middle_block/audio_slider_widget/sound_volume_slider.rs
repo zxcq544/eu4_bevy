d3bd4533d5e5c960d2f7618416ee9230733dfa26
Sergey Anisimov <zxcq544@yandex.ru>
2026-09-17 15:57:48 +0300

    refactor rename audio channel funcs

--- src/plugins/options/components/options_middle_block/audio_slider_widget/sound_volume_slider.rs
+++ src/plugins/options/components/options_middle_block/audio_slider_widget/sound_volume_slider.rs
@@ -17,11 +17,16 @@ pub fn sound_volume_slider(
 ) -> impl Bundle {
     (
         audio_channel,
-        slider(0.0, 1.0, audio_channel.get(&settings), ui_resources),
+        slider(
+            0.0,
+            1.0,
+            audio_channel.get_volume_from_settings(&settings),
+            ui_resources,
+        ),
         observe(slider_self_update),
         observe(
             move |change: On<ValueChange<f32>>, mut settings: ResMut<Settings>| {
-                audio_channel.set(&mut settings, change.value);
+                audio_channel.set_volume_in_settings(&mut settings, change.value);
             },
         ),
     )
--- src/shared/audio_channel/audio_channel.rs
+++ src/shared/audio_channel/audio_channel.rs
@@ -10,14 +10,14 @@ pub enum AudioChannelUIMarker {
 }
 
 impl AudioChannelUIMarker {
-    pub fn get(self, s: &Settings) -> f32 {
+    pub fn get_volume_from_settings(self, s: &Settings) -> f32 {
         match self {
             Self::Master => s.volume_settings.get_master_volume(),
             Self::Music => s.volume_settings.get_music_volume(),
             Self::Sfx => s.volume_settings.get_sfx_volume(),
         }
     }
-    pub fn set(self, s: &mut Settings, v: f32) {
+    pub fn set_volume_in_settings(self, s: &mut Settings, v: f32) {
         match self {
             Self::Master => s.volume_settings.set_master_volume(v),
             Self::Music => s.volume_settings.set_music_volume(v),
